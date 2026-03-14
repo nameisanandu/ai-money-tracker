87452d39b6b7e74140ab0b3fd9f63c68a46479f4
Your Name <anandumscai@gmail.com>
2026-03-14 11:54:13 +0530

    Configure PostgreSQL database for Render

--- money_tracker/settings.py
+++ money_tracker/settings.py
@@ -4,6 +4,8 @@ Django settings for AI Money Tracker project.
 
 import os
 from pathlib import Path
+import dj_database_url
+
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -57,11 +59,20 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'money_tracker.wsgi.application'
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.config(
+        default=os.environ.get("DATABASE_URL"),
+        conn_max_age=600
+    )
 }
 
 AUTH_PASSWORD_VALIDATORS = [
